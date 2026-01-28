17fa2e2d41b8ddd0d2d946e3e98e94863882936a
iuri lopes <iurigabriellopes30@gmail.com>
2026-01-28 14:08:46 -0300

    atualizaçao

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from db import Base, engine
+from backend.db import Base, engine
 from routers import auth, couriers, orders, restaurants
 
 @asynccontextmanager
